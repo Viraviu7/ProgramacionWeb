c4f913cffbed3d9cc267c8ccfb38a25b3f5d4400
Viraviu7 <vvillarreal@unimagdalena.edu.co>
2024-03-05 16:39:01 -0500

    Cambio nombre de la clase de prueba

--- src/test/java/programacionweb/springback/SpringBackApplicationTests.java
+++ src/test/java/programacionweb/springback/SpringBackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProgWebP1BackApplicationTests {
+class SpringBackApplicationTests {
 
 	@Test
 	void contextLoads() {
